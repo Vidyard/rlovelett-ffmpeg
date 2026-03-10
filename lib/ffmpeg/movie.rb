0c6091304619844d0d638a5d666f150754b44db9
MassimoDeLuca <massimo.deluca@vidyard.com>
2026-03-09 21:11:12 -0400

    fix

--- lib/ffmpeg/movie.rb
+++ lib/ffmpeg/movie.rb
@@ -62,7 +62,7 @@ module FFMPEG
         @duration = 0
       else
         video_streams = metadata[:streams].select { |stream| stream.key?(:codec_type) and stream[:codec_type] === 'video' }
-        audio_streams = metadata[:streams].select { |stream| stream.key?(:codec_type) and stream[:codec_type] === 'audio' and stream[:codec_name] }
+        audio_streams = metadata[:streams].select { |stream| stream.key?(:codec_type) and stream[:codec_type] === 'audio' and stream[:codec_name] != 'none' }
 
         data_streams = metadata[:streams].select { |stream| stream.key?(:codec_type) and stream[:codec_type] === 'data' }
 
